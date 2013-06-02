93589848eefe1297355dc7282442d31b130af5ec
Jess Scholtes <jess.scholtes@jscholtes.eu>
2013-06-03 00:01:48 +0200

    added avg load function

---
+++ load.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ut1=`uptime | awk '{ print $(NF-2) }' | sed 's/,/\./g'`
+ut2=`uptime | awk '{ print $(NF-1) }' | sed 's/,/\./g'`
+ut3=`uptime | awk '{ print $(NF)   }' | sed 's/,/\./g'`
+
+time=`date "+%H:%M"`
+new1="                    { \"title\" : \"$time\", \"value\" : $ut1},"
+new1="                    { \"title\" : \"$time\", \"value\" : $ut2},"
+new2="                    { \"title\" : \"$time\", \"value\" : $ut3},"
+awk 'NR != 9' load.json > .load.tmp && mv .load.tmp load.json
+awk -v n="$new1" 'NR == 32 {print n} {print}' load.json > .load.tmp && mv .load.tmp load.json
+awk 'NR != 38' load.json > .load.tmp && mv .load.tmp load.json
+awk -v n="$new2" 'NR == 61 {print n} {print}' load.json > .load.tmp && mv .load.tmp load.json
+awk 'NR != 67' load.json > .load.tmp && mv .load.tmp load.json
+awk -v n="$new3" 'NR == 90 {print n} {print}' load.json > .load.tmp && mv .load.tmp load.json
