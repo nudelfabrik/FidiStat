32cddcbd9e5df72d5155105e9df72ce85e9f1a59
Jess Scholtes <jess.scholtes@jscholtes.eu>
2013-06-02 23:43:06 +0200

    changed order of deletions

--- cpu.sh
+++ cpu.sh
@@ -5,7 +5,7 @@ time=`date "+%H:%M"`
 new1="                    { \"title\" : \"$time\", \"value\" : $cpu1},"
 new2="                    { \"title\" : \"$time\", \"value\" : $cpu2},"
 awk 'NR != 9' cpu.json > .cpu.tmp && mv .cpu.tmp cpu.json
-awk 'NR != 37' cpu.json > .cpu.tmp && mv .cpu.tmp cpu.json
 awk -v n="$new1" 'NR == 32 {print n} {print}' cpu.json > .cpu.tmp && mv .cpu.tmp cpu.json
+awk 'NR != 38' cpu.json > .cpu.tmp && mv .cpu.tmp cpu.json
 awk -v n="$new2" 'NR == 61 {print n} {print}' cpu.json > .cpu.tmp && mv .cpu.tmp cpu.json
 
