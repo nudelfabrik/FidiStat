1bd08386c83850e594c5c772c7325826c70ad522
Nudelfabrik <nudelfabrik@googlemail.com>
2013-06-04 13:51:16 +0200

    min/max at load.json

--- load.sh
+++ load.sh
@@ -7,9 +7,9 @@ time=`date "+%H:%M"`
 new1="                    { \"title\" : \"$time\", \"value\" : $ut1},"
 new2="                    { \"title\" : \"$time\", \"value\" : $ut2},"
 new3="                    { \"title\" : \"$time\", \"value\" : $ut3},"
-awk 'NR != 9' load.json > .load.tmp && mv .load.tmp load.json
-awk -v n="$new1" 'NR == 32 {print n} {print}' load.json > .load.tmp && mv .load.tmp load.json
-awk 'NR != 38' load.json > .load.tmp && mv .load.tmp load.json
-awk -v n="$new2" 'NR == 61 {print n} {print}' load.json > .load.tmp && mv .load.tmp load.json
-awk 'NR != 67' load.json > .load.tmp && mv .load.tmp load.json
-awk -v n="$new3" 'NR == 90 {print n} {print}' load.json > .load.tmp && mv .load.tmp load.json
+awk 'NR != 13' load.json > .load.tmp && mv .load.tmp load.json
+awk -v n="$new1" 'NR == 36 {print n} {print}' load.json > .load.tmp && mv .load.tmp load.json
+awk 'NR != 42' load.json > .load.tmp && mv .load.tmp load.json
+awk -v n="$new2" 'NR == 65 {print n} {print}' load.json > .load.tmp && mv .load.tmp load.json
+awk 'NR != 71' load.json > .load.tmp && mv .load.tmp load.json
+awk -v n="$new3" 'NR == 94 {print n} {print}' load.json > .load.tmp && mv .load.tmp load.json
