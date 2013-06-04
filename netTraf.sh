5ac3c37df0e27b21dc7cd72d91f7f8871b09c7a7
Jess Scholtes <jess.scholtes@jscholtes.eu>
2013-06-04 13:47:11 +0200

    fixed netTraf.sh

--- netTraf.sh
+++ netTraf.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 stat=`ifstat -i re0 1 1 | tail -1`
-in=`awk '{print $1}' $stat`
-out=`awk '{print $2}' $stat`
+in=`echo $stat | awk '{print $1}'`
+out=`echo $stat | awk '{print $2}'`
 
 time=`date "+%H:%M"`
 new1="                    { \"title\" : \"$time\", \"value\" : $in},"
 new2="                    { \"title\" : \"$time\", \"value\" : $out},"
-awk 'NR != 9' load.json > .load.tmp && mv .load.tmp load.json
-awk -v n="$new1" 'NR == 32 {print n} {print}' load.json > .load.tmp && mv .load.tmp load.json
-awk 'NR != 38' load.json > .load.tmp && mv .load.tmp load.json
-awk -v n="$new2" 'NR == 61 {print n} {print}' load.json > .load.tmp && mv .load.tmp load.json
+awk 'NR != 9' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
+awk -v n="$new1" 'NR == 32 {print n} {print}' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
+awk 'NR != 38' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
+awk -v n="$new2" 'NR == 61 {print n} {print}' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
