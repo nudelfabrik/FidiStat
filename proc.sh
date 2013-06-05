7a3b4a43a15e75086a6dcbd6d4a3bfcf212f44fd
Jess Scholtes <jess.scholtes@jscholtes.eu>
2013-06-05 23:24:03 +0200

    Never copy/paste :) changed load.tmp to proc.tmp

--- proc.sh
+++ proc.sh
@@ -12,11 +12,11 @@ ru="                    { \"title\" : \"$time\", \"value\" : $ruVal},"
 dw="                    { \"title\" : \"$time\", \"value\" : $dwVal},"
 pw="                    { \"title\" : \"$time\", \"value\" : $slVal},"
 sl="                    { \"title\" : \"$time\", \"value\" : $slVal},"
-awk 'NR != 9' load.json > .load.tmp && mv .load.tmp load.json
-awk -v n="$ru" 'NR == 32 {print n} {print}' load.json > .load.tmp && mv .load.tmp load.json
-awk 'NR != 38' load.json > .load.tmp && mv .load.tmp load.json
-awk -v n="$dw" 'NR == 61 {print n} {print}' load.json > .load.tmp && mv .load.tmp load.json
-awk 'NR != 67' load.json > .load.tmp && mv .load.tmp load.json
-awk -v n="$pw" 'NR == 90 {print n} {print}' load.json > .load.tmp && mv .load.tmp load.json
-awk 'NR != 96' load.json > .load.tmp && mv .load.tmp load.json
-awk -v n="$sl" 'NR == 119 {print n} {print}' load.json > .load.tmp && mv .load.tmp load.json
+awk 'NR != 9' proc.json > .proc.tmp && mv .proc.tmp proc.json
+awk -v n="$ru" 'NR == 32 {print n} {print}' proc.json > .proc.tmp && mv .proc.tmp proc.json
+awk 'NR != 38' proc.json > .proc.tmp && mv .proc.tmp proc.json
+awk -v n="$dw" 'NR == 61 {print n} {print}' proc.json > .proc.tmp && mv .proc.tmp proc.json
+awk 'NR != 67' proc.json > .proc.tmp && mv .proc.tmp proc.json
+awk -v n="$pw" 'NR == 90 {print n} {print}' proc.json > .proc.tmp && mv .proc.tmp proc.json
+awk 'NR != 96' proc.json > .proc.tmp && mv .proc.tmp proc.json
+awk -v n="$sl" 'NR == 119 {print n} {print}' proc.json > .proc.tmp && mv .proc.tmp proc.json
