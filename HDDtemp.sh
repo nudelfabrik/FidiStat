a9d88b484e72cf9cc493f113dbf667360ab70fec
Nudelfabrik <nudelfabrik@googlemail.com>
2013-06-06 11:07:34 +0200

    HDDtemp for loop didnt work on freenas

--- HDDtemp.sh
+++ HDDtemp.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
-j=0
-smartopt=`echo $@ | awk '{opt="-n standby"; if(match(tolower($1),'/-w/')) opt=""; print opt; }'`
-for i in $(sysctl -n kern.disks | awk '{for (i=NF; i!=0 ; i--) if (match($i, '/ada/')) print $i }') 
-	do
-        j=$((j + 1))      
-  		temps[$j]=`smartctl -a /dev/$i | awk '/Temperature_Celsius/{print $0}' | awk '{print $10}'`
-	done
+   
+temps1=`smartctl -a /dev/ada0 | awk '/Temperature_Celsius/{print $0}' | awk '{print $10}'`
+temps2=`smartctl -a /dev/ada1 | awk '/Temperature_Celsius/{print $0}' | awk '{print $10}'`
+temps3=`smartctl -a /dev/ada2 | awk '/Temperature_Celsius/{print $0}' | awk '{print $10}'`
+temps4=`smartctl -a /dev/ada3 | awk '/Temperature_Celsius/{print $0}' | awk '{print $10}'`
+temps5=`smartctl -a /dev/ada4 | awk '/Temperature_Celsius/{print $0}' | awk '{print $10}'`
 
 time=`date "+%H:%M"`
-temp1=${temps[1]}
-temp2=${temps[2]}
-temp3=${temps[3]}
-temp4=${temps[4]}
-temp5=${temps[5]}
-new1="                    { \"title\" : \"$time\", \"value\" : $temp1},"
-new2="                    { \"title\" : \"$time\", \"value\" : $temp2},"
-new3="                    { \"title\" : \"$time\", \"value\" : $temp2},"
-new4="                    { \"title\" : \"$time\", \"value\" : $temp4},"
-new5="                    { \"title\" : \"$time\", \"value\" : $temp5},"
+new1="                    { \"title\" : \"$time\", \"value\" : $temps1},"
+new2="                    { \"title\" : \"$time\", \"value\" : $temps2},"
+new3="                    { \"title\" : \"$time\", \"value\" : $temps3},"
+new4="                    { \"title\" : \"$time\", \"value\" : $temps4},"
+new5="                    { \"title\" : \"$time\", \"value\" : $temps5},"
 awk 'NR != 9' HDDtemp.json > .HDDtemp.tmp && mv .HDDtemp.tmp HDDtemp.json
 awk -v n="$new1" 'NR == 32 {print n} {print}' HDDtemp.json > .HDDtemp.tmp && mv .HDDtemp.tmp HDDtemp.json
 awk 'NR != 38' HDDtemp.json > .HDDtemp.tmp && mv .HDDtemp.tmp HDDtemp.json
