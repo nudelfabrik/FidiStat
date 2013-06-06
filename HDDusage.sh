67031c66769d87a72ad2de2c1cdba4d86f91c058
Jess Scholtes <jess.scholtes@jscholtes.eu>
2013-06-06 09:01:46 +0200

    HDDusage.sh printed the command directly to .json, now fixed"

--- HDDusage.sh
+++ HDDusage.sh
@@ -14,15 +14,17 @@ echo "{
 " >> HDDusage.json
 tmp=`df -H | grep ^Data/Dokumente | awk '{ gsub("T", "000000000", $3); gsub("G", "000000", $3); gsub("M", "000", $3); gsub("k", "", $3); $3}'`
 echo "                    { \"title\" : \"Dokumente\", \"value\" : $tmp}," >> $filename
-tmp=`f -H | grep ^Data/Filme | awk '{ gsub("T", "000000000", $3); gsub("G", "000000", $3); gsub("M", "000", $3); gsub("k", "", $3); $3}' >> $filename` 
+
+tmp=`f -H | grep ^Data/Filme | awk '{ gsub("T", "000000000", $3); gsub("G", "000000", $3); gsub("M", "000", $3); gsub("k", "", $3); $3}'` 
 echo "                    { \"title\" : \"Filme\", \"value\" : $tmp}," >> $filename
-tmp=`f -H | grep ^Data/Musik | awk '{ gsub("T", "000000000", $3); gsub("G", "000000", $3); gsub("M", "000", $3); gsub("k", "", $3); $3}' >> $filename`
+
+tmp=`f -H | grep ^Data/Musik | awk '{ gsub("T", "000000000", $3); gsub("G", "000000", $3); gsub("M", "000", $3); gsub("k", "", $3); $3}'`
 echo "                    { \"title\" : \"Musik\", \"value\" : $tmp}," >> $filename
 
-tmp=`f -H | grep ^Data/Programme | awk '{ gsub("T", "000000000", $3); gsub("G", "000000", $3); gsub("M", "000", $3); gsub("k", "", $3); $3}' >> $filename`
+tmp=`f -H | grep ^Data/Programme | awk '{ gsub("T", "000000000", $3); gsub("G", "000000", $3); gsub("M", "000", $3); gsub("k", "", $3); $3}'`
 echo "                    { \"title\" : \"Programme\", \"value\" : $tmp}," >> $filename
 
-tmp=`f -H | grep ^Data/Spiele | awk '{ gsub("T", "000000000", $3); gsub("G", "000000", $3); gsub("M", "000", $3); gsub("k", "", $3); $3}' >> $filename`
+tmp=`f -H | grep ^Data/Spiele | awk '{ gsub("T", "000000000", $3); gsub("G", "000000", $3); gsub("M", "000", $3); gsub("k", "", $3); $3}'`
 echo "                    { \"title\" : \"Spiele\", \"value\" : $tmp}," >> $filename
 
 tmp=`f -H | grep ^Data/Filme | awk '{ gsub("T", "000000000", $2); gsub("G", "000000", $2); gsub("M", "000", $2); gsub("k", "", $2); $2}' >> $filename`
