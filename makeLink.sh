7fa75c43732878782fc66db9f0af70c284ca4d06
Nudelfabrik <nudelfabrik@googlemail.com>
2013-06-06 09:44:17 +0200

    alternative for makeLink.sh link

--- makeLink.sh
+++ makeLink.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
-if [[ $# != 3 ]]
+if [[ $# == 3 ]]
 then
-    echo "I need 3 parameters, User Password and Server address"
-else
-    rm statboardLinks.html
-    touch statboardLinks.html
+    rm statboardlinks.html
+    touch statboardlinks.html
 
     echo "<html>
     <body>
-    <h1>FidiStat</h1>
+    <h1>fidistat</h1>
 
-    <p>FreeNAS monitoring for Panic&#8217;s <a href="http://www.panic.com/statusboard/">staus board</a></p>
+    <p>freenas monitoring for panic&#8217;s <a href="http://www.panic.com/statusboard/">staus board</a></p>
 
     <h2>Click these links on an iPad:</h2>
     " >> statboardLinks.html
@@ -21,4 +19,27 @@ else
 
     echo "</body>
     </html>" >>statboardLinks.html
+elif [[ $# == 1]]
+then
+    rm statboardlinks.html
+    touch statboardlinks.html
+
+    echo "<html>
+    <body>
+    <h1>fidistat</h1>
+
+    <p>freenas monitoring for panic&#8217;s <a href="http://www.panic.com/statusboard/">staus board</a></p>
+
+    <h2>Click these links on an iPad:</h2>
+    " >> statboardLinks.html
+    find *.json | while read i
+    do
+        echo "<a href=\"panicboard://$1\">$i</a><br>" >> statboardLinks.html
+    done
+
+    echo "</body>
+    </html>" >>statboardLinks.html
+else
+    echo "You need either 3 parameters, ftp://<User>:<Password>@<Server address>/"
+    echo "or 1 parameter for personal adress (\" \" escaped)"
 fi
