c74bf74fefea4c30e9be7e5062f2ae18c635432d
Nudelfabrik <nudelfabrik@googlemail.com>
2013-06-06 15:25:47 +0200

    added a refresh rate to all .json and modified .sh fixed #5

--- cpu.sh
+++ cpu.sh
@@ -4,8 +4,8 @@ cpu2=`sysctl -n dev.cpu.1.temperature | sed 's/C//g'`
 time=`date "+%H:%M"`
 new1="                    { \"title\" : \"$time\", \"value\" : $cpu1},"
 new2="                    { \"title\" : \"$time\", \"value\" : $cpu2},"
-awk 'NR != 9' cpu.json > .cpu.tmp && mv .cpu.tmp cpu.json
-awk -v n="$new1" 'NR == 32 {print n} {print}' cpu.json > .cpu.tmp && mv .cpu.tmp cpu.json
-awk 'NR != 38' cpu.json > .cpu.tmp && mv .cpu.tmp cpu.json
-awk -v n="$new2" 'NR == 61 {print n} {print}' cpu.json > .cpu.tmp && mv .cpu.tmp cpu.json
+awk 'NR != 10' cpu.json > .cpu.tmp && mv .cpu.tmp cpu.json
+awk -v n="$new1" 'NR == 33 {print n} {print}' cpu.json > .cpu.tmp && mv .cpu.tmp cpu.json
+awk 'NR != 39' cpu.json > .cpu.tmp && mv .cpu.tmp cpu.json
+awk -v n="$new2" 'NR == 62 {print n} {print}' cpu.json > .cpu.tmp && mv .cpu.tmp cpu.json
 
