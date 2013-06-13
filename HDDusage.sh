7a3d449a1066a388ee796e10b47b4a3d2857f83d
Jess Scholtes <jess.scholtes@jscholtes.eu>
2013-06-13 13:34:09 +0200

    finished HDDusage.sh

--- HDDusage.sh
+++ HDDusage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 filename="HDDusage.json"
-
-echo "Size; HDD Usage" >> $filename
+rm $filename
+touch $filename
+#echo "Size; HDD Usage" >> $filename
 echo "{
     \"graph\" : {
         \"title\" : \"HDD usage\",
@@ -12,23 +12,23 @@ echo "{
                 \"title\": \"absolute\",
                 \"datapoints\" : [
 " >> HDDusage.json
-tmp=`df -H | grep ^Data/Dokumente | awk '{ gsub("T", "000000000", $3); gsub("G", "000000", $3); gsub("M", "000", $3); gsub("k", "", $3); $3}'`
+tmp=`df | grep ^Data/Dokumente | awk '{print $3}'`
 echo "                    { \"title\" : \"Dokumente\", \"value\" : $tmp}," >> $filename
 
-tmp=`f -H | grep ^Data/Filme | awk '{ gsub("T", "000000000", $3); gsub("G", "000000", $3); gsub("M", "000", $3); gsub("k", "", $3); $3}'` 
+tmp=`df | grep ^Data/Filme | awk '{print $3}'` 
 echo "                    { \"title\" : \"Filme\", \"value\" : $tmp}," >> $filename
 
-tmp=`f -H | grep ^Data/Musik | awk '{ gsub("T", "000000000", $3); gsub("G", "000000", $3); gsub("M", "000", $3); gsub("k", "", $3); $3}'`
-echo "                    { \"title\" : \"Musik\", \"value\" : $tmp}," >> $filename
+tmp=`df | grep ^Data/Audio | awk '{print $3}'`
+echo "                    { \"title\" : \"Audio\", \"value\" : $tmp}," >> $filename
 
-tmp=`f -H | grep ^Data/Programme | awk '{ gsub("T", "000000000", $3); gsub("G", "000000", $3); gsub("M", "000", $3); gsub("k", "", $3); $3}'`
+tmp=`df | grep ^Data/Programme | awk '{print $3}'`
 echo "                    { \"title\" : \"Programme\", \"value\" : $tmp}," >> $filename
 
-tmp=`f -H | grep ^Data/Spiele | awk '{ gsub("T", "000000000", $3); gsub("G", "000000", $3); gsub("M", "000", $3); gsub("k", "", $3); $3}'`
+tmp=`df | grep ^Data/Spiele | awk '{print $3}'`
 echo "                    { \"title\" : \"Spiele\", \"value\" : $tmp}," >> $filename
 
-tmp=`f -H | grep ^Data/Filme | awk '{ gsub("T", "000000000", $2); gsub("G", "000000", $2); gsub("M", "000", $2); gsub("k", "", $2); $2}' >> $filename`
-echo "                    { \"title\" : \"Filme\", \"value\" : $tmp}," >> $filename
+tmp=`df | grep ^Data/Filme | awk '{print $3}'`
+echo "                    { \"title\" : \"Gesamt\", \"value\" : $tmp}," >> $filename
 
 
 echo "
