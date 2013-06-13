57f200dbf413f0d744637582d7445b6c69caaf23
Fidi <kevinfiedler93@googlemail.com>
2013-06-13 17:05:39 +0200

    modified liveNet.sh

--- liveNet.sh
+++ liveNet.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-totalIn=0
-totalOut=0
+totalIn=0.0
+totalOut=0.0
 for i in {1..60}
 do
     sleep 1
@@ -9,8 +9,8 @@ do
     in=`echo $stat | awk '{print $1}'`
     out=`echo $stat | awk '{print $2}'`
 
-    totalIn=$(($totalIn + $in))
-    totalOut=$(($totalOut + $out))
+    totalIn=`echo "$totalIn + $in" | bc`
+    totalOut=`echo "$totalOut + $out" | bc`
 
     time=`date "+%H:%M"`
     new1="                    { \"title\" : \"$time\", \"value\" : $in},"
@@ -21,9 +21,9 @@ do
     awk -v n="$new2" 'NR == 616 {print n} {print}' liveNet.json > .liveNet.tmp && mv .liveNet.tmp liveNet.json
 done
 
-new1="                    { \"title\" : \"$time\", \"value\" : $TotalIn},"
-new2="                    { \"title\" : \"$time\", \"value\" : $TotalOut},"
+totin=`echo "                    { \"title\" : \"$time\", \"value\" : $totalIn}," | sed "s/ \./ 0./g"`
+totout=`echo "                    { \"title\" : \"$time\", \"value\" : $totalOut}," | sed "s/ \./ 0\./g" `
 awk 'NR != 10' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
-awk -v n="$new1" 'NR == 33 {print n} {print}' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
+awk -v n="$totin" 'NR == 33 {print n} {print}' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
 awk 'NR != 39' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
-awk -v n="$new2" 'NR == 62 {print n} {print}' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
+awk -v n="$totout" 'NR == 62 {print n} {print}' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
