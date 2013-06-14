588ef19c03e58e214c0796cb0708fcfc42338496
Nudelfabrik <nudelfabrik@googlemail.com>
2013-06-14 14:41:48 +0200

    added color to network

--- liveNet.sh
+++ liveNet.sh
@@ -15,15 +15,15 @@ do
     time=`date "+%H:%M"`
     new1="                    { \"title\" : \"$time\", \"value\" : $in},"
     new2="                    { \"title\" : \"$time\", \"value\" : $out},"
-    awk 'NR != 10' liveNet.json > .liveNet.tmp && mv .liveNet.tmp liveNet.json
-    awk -v n="$new1" 'NR == 310 {print n} {print}' liveNet.json > .liveNet.tmp && mv .liveNet.tmp liveNet.json
-    awk 'NR != 316' liveNet.json > .liveNet.tmp && mv .liveNet.tmp liveNet.json
-    awk -v n="$new2" 'NR == 616 {print n} {print}' liveNet.json > .liveNet.tmp && mv .liveNet.tmp liveNet.json
+    awk 'NR != 11' liveNet.json > .liveNet.tmp && mv .liveNet.tmp liveNet.json
+    awk -v n="$new1" 'NR == 311 {print n} {print}' liveNet.json > .liveNet.tmp && mv .liveNet.tmp liveNet.json
+    awk 'NR != 318' liveNet.json > .liveNet.tmp && mv .liveNet.tmp liveNet.json
+    awk -v n="$new2" 'NR == 618 {print n} {print}' liveNet.json > .liveNet.tmp && mv .liveNet.tmp liveNet.json
 done
 
 totin=`echo "                    { \"title\" : \"$time\", \"value\" : $totalIn}," | sed "s/ \./ 0./g"`
 totout=`echo "                    { \"title\" : \"$time\", \"value\" : $totalOut}," | sed "s/ \./ 0\./g" `
-awk 'NR != 10' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
-awk -v n="$totin" 'NR == 33 {print n} {print}' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
-awk 'NR != 39' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
-awk -v n="$totout" 'NR == 62 {print n} {print}' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
+awk 'NR != 11' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
+awk -v n="$totin" 'NR == 34 {print n} {print}' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
+awk 'NR != 41' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
+awk -v n="$totout" 'NR == 64 {print n} {print}' netTraf.json > .netTraf.tmp && mv .netTraf.tmp netTraf.json
