8160725bb9b0e125f3cda2fbb8a6976075380da9
Nudelfabrik <nudelfabrik@googlemail.com>
2013-06-15 13:14:52 +0200

    fixed liveNet.sh

--- liveNet.sh
+++ liveNet.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 totalIn=0.0
 totalOut=0.0
-for i in {1..60}
+
+
+for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59
 do
     sleep 1
+	
     stat=`ifstat -i re0 1 1 | tail -1`
     in=`echo $stat | awk '{print $1}'`
     out=`echo $stat | awk '{print $2}'`
