611f07bed158ebf5cef440d0c63a5a29fd38a96e
Jess Scholtes <jess.scholtes@jscholtes.eu>
2013-08-11 17:20:45 +0200

    finally regexing works. Needs Cleanup

--- janssonTest.c
+++ janssonTest.c
@@ -20,26 +20,25 @@ int main(int argc, const char *argv[])
     int status;
     char source[1024];
 char * path = "0:36  up 23 days,  9:51, 3 users, load averages: 0,85 0,59 0,53";
+const char *srcpnt = source;
     size_t maxGroups = 3;
-    regmatch_t pmatch[maxGroups];
+    regmatch_t pmatch;
 
     //char *cmmd = malloc(256);
     //sprintf(cmmd, "uptime %s", PATH);
     fp = popen("uptime", "r");
     //free(cmmd);
-    retex = regcomp( &regex, "( [0-9]?[0-9],[0-9][0-9]){3,3}", REG_EXTENDED);
+    retex = regcomp( &regex, "[0-9]?[0-9],[0-9][0-9]", REG_EXTENDED);
     //if( retex ){ fprintf(stderr, "Could not compile regex\n"); exit(1); }
+    char res [1024];
             
             
     while (fgets(source, sizeof(source)-1, fp) != NULL) {
-          if (regexec(&regex, source, maxGroups, pmatch, 0) == 0) {
-                if (pmatch[0].rm_so == (size_t)-1)
-                    break;  // No more groups
-                char sourceCopy[strlen(source) + 1];
-                strcpy(sourceCopy, source);
-                sourceCopy[pmatch[0].rm_eo] = 0;
-                printf("%s",
-                sourceCopy + pmatch[0].rm_so);
+        printf("%s",source);
+          while (regexec(&regex, srcpnt, 1, &pmatch, 0) == 0) {
+                sprintf(res,"%.*s\n", (int)(pmatch.rm_eo - pmatch.rm_so), &srcpnt[pmatch.rm_so]);
+                srcpnt += pmatch.rm_eo;
+                printf("%s", res);
            }
         
         
