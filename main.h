a9524327ef5d78d733ef2e1facb4a9a9cc24d730
Jess Scholtes <jess.scholtes@jscholtes.eu>
2013-08-12 00:52:08 +0200

    added more to config

--- main.h
+++ main.h
@@ -5,14 +5,15 @@
 #define CFG_FILE "config.cfg"
 #define MAXCOUNT 30
 #define OUTPUT_SIZE 1024
+#define MAXSTAT 10
 
 struct statStruct {
     int enabled;
     const char *name;
     const char *cmmd;
     const char *regex;
-    char raw[OUTPUT_SIZE] ;
-    double  result[10];
+    char raw[OUTPUT_SIZE];
+    double  result[MAXSTAT];
 
 };
 typedef struct statStruct Status;
