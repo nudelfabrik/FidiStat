17068771d85add3905b5d5770acbd0b49a3f4e82
Nudelfabrik <nudelfabrik@googlemail.com>
2013-08-11 20:00:42 +0200

    working version

--- regex.c
+++ regex.c
@@ -8,8 +8,8 @@
 #include <stdlib.h>
 #include <regex.h>
 #include <string.h>
-#include "regex.h"
 #include "main.h"
+#include "regex.h"
 
 struct statStruct stats[10];
 
@@ -18,21 +18,29 @@ void regexing(int i) {
     int retex;
     regmatch_t pmatch;
     const char *rawPnt = stats[i].raw;
+    char res[OUTPUT_SIZE];
+    int j = 0;
 
     retex = regcomp( &regex, stats[i].regex, REG_EXTENDED);
     if( retex ){ fprintf(stderr, "Could not compile regex\n"); exit(1); }
         while (regexec(&regex, rawPnt, 1, &pmatch, 0) == 0) {
-            sprintf(stats[i].result, "%s%.*s\n",stats[i].result,  (int)(pmatch.rm_eo - pmatch.rm_so), &rawPnt[pmatch.rm_so]);
+            sprintf(res, "%.*s",  (int)(pmatch.rm_eo - pmatch.rm_so), &rawPnt[pmatch.rm_so]);
+            sanitize(res);
+            stats[i].result[j] = atof(res);
             rawPnt += pmatch.rm_eo;
+            j++;
         }
-    sanitize(i);
 }
 
-void sanitize(int i) {
+void sanitize(char c[OUTPUT_SIZE]) {
     int j;
-    for (j = 0; j < strlen(stats[i].result)+ 1  ; j++) {
-        if (stats[i].result[j] == ',') {
-            stats[i].result[j] = '.';
+    int k;
+    for (k = 0; k < 10; k++) {
+        
+    for (j = 0; j < strlen(c)- 1  ; j++) {
+        if (c[j] == ',') {
+            c[j] = '.';
         }
     }
+        }
 }
