d80b766255c5cbf7a3520db4c9a405c6e91759c0
Nudelfabrik <nudelfabrik@googlemail.com>
2013-08-12 23:10:20 +0200

    Removed old stuff

--- oldStuff/HDDtemp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-   
-temps1=`smartctl -a /dev/ada0 | awk '/Temperature_Celsius/{print $0}' | awk '{print $10}'`
-
-time=`date "+%H:%M"`
-new1="                    { \"title\" : \"$time\", \"value\" : $temps1},"
-awk 'NR != 10' HDDtemp.json > .HDDtemp.tmp && mv .HDDtemp.tmp HDDtemp.json
-awk -v n="$new1" 'NR == 33 {print n} {print}' HDDtemp.json > .HDDtemp.tmp && mv .HDDtemp.tmp HDDtemp.json
