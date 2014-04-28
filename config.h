135807c61d1955e01a1c596b2ad6895649265eef
Benedikt Schotles <nudelfabrik@googlemail.com>
2014-04-28 22:53:11 +0200

    fixed config.h includes

--- config.h
+++ config.h
@@ -1,10 +1,8 @@
 #ifndef CONFIG
 #define CONFIG
-#include <libconfig.h>
 void initConf();
 void destroyConf();
 const char * getConfList(Status *Stat, int i);
-config_setting_t* getSetting(const char * item);
 int getStatNum();
 void getPath();
 void getConfEnable(Status *Stat);
