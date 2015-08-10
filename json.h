48d99d757f42df4494f2c2cc05e9fb2eb4a115b0
Benedikt Scholtes <bene.scholtes@fourtytwo.xyz>
2015-08-10 16:03:17 +0200

    makeJansson now only creates array of key/value pairs

--- json.h
+++ json.h
@@ -10,7 +10,8 @@ void dumpJSON(json_t *root, const char *name);
 void sendJSON(json_t *root, const char *name);
 json_t* getDataSequences(json_t* graph);
 json_t* getSingleSeqeunce(json_t* sequences, int i);
-const char* getTitle(json_t* graph);
+const char* getTitle(json_t* root);
+const char* getType(json_t* root);
 int check(json_t* object);
 void printError(const char* name);
 
