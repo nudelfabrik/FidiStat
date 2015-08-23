a3ef26a0a5b5530ca87d5c8db19deeec4628a346
Benedikt Scholtes <bene.scholtes@fourtytwo.xyz>
2015-08-23 20:48:06 +0200

    fixed #36

--- server.h
+++ server.h
@@ -6,6 +6,7 @@ void server();
 void worker(int connfd, struct tls* ctx);
 int initTLS_S(struct tls* ctx);
 struct tls_config* tlsServer_conf;
+struct addrinfo* getAddrInfo();
 const char *port_S;
 
 #endif
