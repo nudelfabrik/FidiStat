2f0a9ee571b05cff0e2a04b23d244783467fcd48
Jess Scholtes <jess.scholtes@jscholtes.eu>
2015-08-25 20:34:06 +0200

    createFile now needs connectionType

--- bootstrap.h
+++ bootstrap.h
@@ -5,7 +5,7 @@
 #include <jansson.h>
 
 void bootstrap(Status* status);
-void createFile(Status* status);
+void createFile(Status* status, int type);
 int checkForBootstrap(const char* name);
 
 json_t* getDisplaySettings(int id, const char* subSetting);
