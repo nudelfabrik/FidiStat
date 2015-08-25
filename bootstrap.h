b4aa615d15c348a3445360d52ebf62a44f57b6ab
Benedikt Scholtes <bene.scholtes@fourtytwo.xyz>
2015-08-25 17:02:10 +0200

    fixed bootstrap problem. fix #39

--- bootstrap.h
+++ bootstrap.h
@@ -9,9 +9,6 @@ void createFile(Status* status);
 int checkForBootstrap(const char* name);
 
 json_t* getDisplaySettings(const char* name, const char* subSetting);
-void addNewSubSetting(json_t* graph, const char* subObj);
-void addNewString(json_t* graph, const char* key, const char* value, const char* subObj);
-void addNewInt(json_t* graph, const char* key, int value, const char* subObj);
 
 json_t* getSequences(const char* name);
 
