4ae033e8044e21339fb15c66cbe337f7d560cf0c
Benedikt Scholtes <bene.scholtes@fourtytwo.xyz>
2015-12-13 22:54:23 +0100

    first, non working prototype. reads fail

--- server.h
+++ server.h
@@ -5,11 +5,15 @@
 
 struct connInfo {
     int c_fd;
+    short header;
+    uint16_t expect;
     char* buffer;
+    size_t read;
     struct tls* cctx;
 };
 typedef struct connInfo Cinfo;
 void server();
+void work(Cinfo* cid, size_t backlog);
 void worker(int connfd, struct tls* ctx);
 void addEvent(int kq, uintptr_t ident, short filter, u_short flags, u_int fflags, intptr_t data, void *udata);
 
